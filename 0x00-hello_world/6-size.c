d3f26f8f777a54bd1dd1a4f7f54c7baadb879911
Queenbgr <barengramao274@gmail.com>
2023-06-15 16:11:17 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int c;
 long long int d;
 float g;
 
-printf("Size of a char: %lubyte(s)\n", (unsigned long)sizeof(a));
-printf("Size of int: %lubyte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lubyte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lubyte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lubyte(s)\n", (unsigned long)sizeof(g));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
 return (0);
 }
